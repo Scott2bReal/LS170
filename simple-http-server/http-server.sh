3edb01f8782cba1dc4a3975826104a63fb7838ec
Scott Hoecker <hoeckers@gmail.com>
2022-05-13 15:44:36 -0500

    Fix formatting http server

--- simple-http-server/http-server.sh
+++ simple-http-server/http-server.sh
@@ -1,35 +1,31 @@
 #!/usr/bin/bash
 
-function server () {
-  while true
-  do
-    message_arr=()
-    check=true
-    while $check
-    do
-      read line
-      message_arr+=($line)
-      if [[ "${#line}" -eq 1 ]]
-      then
-        check=false
-      fi
-    done
-    method=${message_arr[0]}
-    path=${message_arr[1]}
-    if [[ $method = GET ]]
-    then
-      if [[ -f ./www/$path ]]
-      then
-        echo -en "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c ./www/$path)\r\n\r\n"; cat ./www/$path
-      else
-        echo -en "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
-      fi
-    else
-      echo -en "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n"
-    fi
-  done
+function server() {
+	while true; do
+		message_arr=()
+		check=true
+		while $check; do
+			read -r line
+			message_arr+=("$line")
+			if [[ "${#line}" -eq 1 ]]; then
+				check=false
+			fi
+		done
+		method=${message_arr[0]}
+		path=${message_arr[1]}
+		if [[ $method = GET ]]; then
+			if [[ -f ./www/$path ]]; then
+				echo -en "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c ./www/"$path")\r\n\r\n"
+				cat ./www/"$path"
+			else
+				echo -en "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+			fi
+		else
+			echo -en "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n"
+		fi
+	done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lvp 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+netcat -lvp 2345 <"&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}"
